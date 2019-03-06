479b7c6aba14f701ccb9d5322a458616ad100948
Kason <718087538@qq.com>
2019-03-06 14:04:15 +0800

    把左列表数据单独抽出来

---
+++ js/left-data.js
@@ -0,0 +1,11 @@
+// 定义一个标题和内容链接的数组
+var _Arry = [
+    {title:'起步', url: 'pages/a.html'},
+    {title:'首页', url: 'pages/a.html'},
+    {title:'购物车', url: 'pages/b.html'},
+    {title:'首页和购物车', url: 'pages/a.html'},
+    {title:'首页和购物车', url: 'pages/a.html'},
+    {title:'首页和购物车', url: 'pages/a.html'},
+    {title:'首页和购物车', url: 'pages/a.html'},
+    {title:'首页和购物车', url: 'pages/b.html'}
+];
