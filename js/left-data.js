b295924b6327a82135ce2f5bf929d91cc1130aa0
Kason <718087538@qq.com>
2019-03-06 17:28:22 +0800

    更新了一下API

--- js/left-data.js
+++ js/left-data.js
@@ -3,9 +3,9 @@ var _Arry = [
     {title:'起步', url: 'pages/a.html'},
     {title:'首页', url: 'pages/a.html'},
     {title:'购物车', url: 'pages/b.html'},
-    {title:'首页和购物车', url: 'pages/a.html'},
-    {title:'首页和购物车', url: 'pages/a.html'},
-    {title:'首页和购物车', url: 'pages/a.html'},
-    {title:'首页和购物车', url: 'pages/a.html'},
-    {title:'首页和购物车', url: 'pages/b.html'}
+    {title:'地址', url: 'pages/address.html'},
+    {title:'我的', url: 'pages/a.html'},
+    {title:'订单详情', url: 'pages/a.html'},
+    {title:'订单列表', url: 'pages/a.html'},
+    {title:'修改资料', url: 'pages/b.html'}
 ];
