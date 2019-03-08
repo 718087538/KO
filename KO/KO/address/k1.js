d79ea49c51d6db167746ff015ba1f0aa3867dfe4
Kason <718087538@qq.com>
2019-03-08 10:33:00 +0800

    曾加了一个底部固定按钮

--- KO/KO/address/k1.js
+++ KO/KO/address/k1.js
@@ -7,10 +7,10 @@ Component({
         ul:{
             type:Array,
             value:[
-                {title:"收货人",placeholder:"请输入内容",value:"KASON"},
-                {title:"联系方式",placeholder:"请输入联系方式",value:"1666666666"},
-                {title:"省 市 区",placeholder:"请输入地址",value:"KK省 KK市 KK区"},
-                { title: "详细地址", placeholder: "请输入详细地址", value: "某某镇某某街道某某号" },
+                {title:"收货人",placeholder:"请输入内容",value:""},
+                {title:"联系方式",placeholder:"请输入联系方式",value:""},
+                {title:"省 市 区",placeholder:"请输入地址",value:""},
+                { title: "详细地址", placeholder: "请输入详细地址", value: "" },
             ]
         }
     },
---
+++ KO/KO/button/button1.js
@@ -0,0 +1,34 @@
+// KO/button/button1.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        backgroundColor:{
+            type:String,
+            value:"#ff759c"
+        },
+        color: {
+            type: String,
+            value: "#ffffff"
+        },
+        name: {
+            type: String,
+            value: "按钮名"
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
