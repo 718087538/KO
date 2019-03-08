6550d173a772ee71df501d8bf44acb3c025d0395
Kason <718087538@qq.com>
2019-03-08 11:32:57 +0800

    添加了3条组件

--- KO/pages/index/index.js
+++ KO/pages/index/index.js
@@ -5,10 +5,10 @@ const app = getApp()
 Page({
   data:{
      ul: [
-          { title: "收货人", placeholder: "请输入内容", value: "KASON" },
-          { title: "联系方式", placeholder: "请输入联系方式", value: "1666666666" },
-          { title: "省 市 区", placeholder: "请输入地址", value: "KK省 KK市 KK区" },
-          { title: "详细地址", placeholder: "请输入详细地址", value: "某某镇某某街道某某号" },
+          { title: "宝宝姓名", placeholder: "请输入内容", value: "KASON" },
+          { title: "出生日期", placeholder: "请输入联系方式", value: "1666666666" },
+         { title: "性别", placeholder: "--请选择--", value: "KK省 KK市 KK区" },
+         { title: "奶粉品牌", placeholder: "请输入宝宝喝的奶粉品牌", value: "某某镇某某街道某某号" },
       ],
       color: "#ffffff",
       backgroundColor: "#ff759c",
