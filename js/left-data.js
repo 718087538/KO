345d7753928896f37c90da28cad83fec3a2861e5
Kason <718087538@qq.com>
2019-03-08 16:45:53 +0800

    添加起步页面，优化其他页面

--- js/left-data.js
+++ js/left-data.js
@@ -1,11 +1,8 @@
 // 定义一个标题和内容链接的数组
 var _Arry = [
-    {title:'起步', url: 'pages/a.html'},
-    {title:'首页', url: 'pages/a.html'},
+    {title:'起步', url: 'pages/start.html'},
+    {title:'标题', url: 'pages/titl.html'},
     {title:'购物车', url: 'pages/b.html'},
     {title:'地址', url: 'pages/address.html'},
-    {title:'我的', url: 'pages/a.html'},
-    {title:'订单详情', url: 'pages/a.html'},
-    {title:'订单列表', url: 'pages/a.html'},
-    {title:'修改资料', url: 'pages/b.html'}
+    {title:'按钮', url: 'pages/button.html'},
 ];
