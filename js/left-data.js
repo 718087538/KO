07d5d88f33b7216403e8b013820f1ac19ec4e508
Kason <718087538@qq.com>
2019-03-11 13:52:22 +0800

    增加了几个表单组件，并创建了表单文件夹

--- js/left-data.js
+++ js/left-data.js
@@ -1,8 +1,9 @@
 // 定义一个标题和内容链接的数组
 var _Arry = [
     {title:'起步', url: 'pages/start.html'},
-    {title:'标题', url: 'pages/titl.html'},
-    {title:'购物车', url: 'pages/b.html'},
+    {title:'标题', url: 'pages/title.html'},
+    {title:'订单', url: 'pages/order.html'},
     {title:'地址', url: 'pages/address.html'},
     {title:'按钮', url: 'pages/button.html'},
+    {title:'表单', url: 'pages/form.html'},
 ];
