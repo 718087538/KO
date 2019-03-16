604fee164e02c3f5cb1a81d3b20893205d9835da
Kason <718087538@qq.com>
2019-03-16 16:14:45 +0800

    又更新了5条

---
+++ KO/KO/form/form9.js
@@ -0,0 +1,32 @@
+// KO/from/form9.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        items: [
+            { name: '血压升高', value: '血压升高' },
+            { name: '血压升高', value: '血压升高', checked: 'true' },
+            { name: '血压升高', value: '血压升高' },
+            { name: '血压升高', value: '血压升高' },
+            { name: '血压升高', value: '血压升高' },
+            { name: '血压升高', value: '血压升高' },
+        ]
+
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
