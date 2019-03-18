d3c021b9512661dc3890a3863fdbc784853677d8
Kason <718087538@qq.com>
2019-03-18 17:17:04 +0800

    又添加了好几条，准备封装k40

---
+++ KO/KO/list/list10.js
@@ -0,0 +1,39 @@
+// KO/list/list10.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        info1: true,
+        info2: false,
+        info3: false,
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        toggle: function () {
+            this.setData({
+                info1: (!this.data.info1)
+            })
+        },
+        toggle2: function () {
+            this.setData({
+                info2: (!this.data.info2)
+            })
+        },
+        toggle3: function () {
+            this.setData({
+                info3: (!this.data.info3)
+            })
+        },
+    }
+})
---
+++ KO/KO/menu/menu1.js
@@ -0,0 +1,34 @@
+// KO/menu/menu1.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        meun: [
+            { class: "meun-img1", src: "/KO/img/Corn.png", title: "杂粮", url: "/pages/data/vegetable" },
+            { class: "meun-img2", src: "/KO/img/Mushroom.png", title: "菌类" },
+            { class: "meun-img3", src: "/KO/img/Crab.png", title: "水产" },
+            { class: "meun-img4", src: "/KO/img/Apple.png", title: "水果" },
+        ],
+        meun2: [
+            { class: "meun-img5", src: "/KO/img/Broccoli.png", title: "蔬菜" },
+            { class: "meun-img6", src: "/KO/img/Artichoke.png", title: "草本滋补" },
+            { class: "meun-img7", src: "/KO/img/Bacon.png", title: "肉类" },
+            { class: "meun-img8", src: "/KO/img/Carrot.png", title: "根菜" },
+        ]
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
