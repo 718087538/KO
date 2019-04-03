6c6956f25ffd78c9e666243338dcbece4e9c59e5
Kason <718087538@qq.com>
2019-04-03 15:08:06 +0800

    js改为引入方式

--- js/hover.js
+++ js/hover.js
@@ -1,5 +1,15 @@
-$(function () {
-    $(".number").mouseover(function () {
-        alert("111111")
+$(function(){
+
+    // 循环添加数组中的内容
+    for (item in component) {
+        $(".box").append(
+            `<div class="img-box"><img class="example" src="${component[item].src}" alt="${component[item].id}">
+<div class="number">${component[item].id}</div></div>
+    `);}
+    // 添加隐藏和显示
+    $(".img-box").mouseover(function () {
+        // this指的就是当前元素
+        $(this).find(".number").css({"display":"block"});
+        $(this).siblings().find(".number").css({"display":"none"});
     })
 })
