6a19420060c7ab7c8475a98a8150c1ada0a17d5d
Kason <718087538@qq.com>
2019-04-22 14:55:55 +0800

    更新了一些组件

---
+++ KO/KO/order/order20+/order7.js
@@ -0,0 +1,28 @@
+// KO/order/order20+/order7.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        goods: [
+            {},
+            {},
+            {},
+            {},
+        ]
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
---
+++ KO/KO/tab/tab0+/tab5.js
@@ -0,0 +1,33 @@
+// KO/tab/tab0+/tab5.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        status: 0
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        clickTab: function (e) {
+            var that = this;
+            if (that.data.status == e.currentTarget.dataset.tabid) {
+                return false;
+            } else {
+                that.setData({
+                    status: e.currentTarget.dataset.tabid
+                })
+            }
+        },
+    }
+})
