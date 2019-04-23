8265049c0f476dd92060f932978350e58db97602
Kason <718087538@qq.com>
2019-04-23 09:58:43 +0800

    更新了一些组件

---
+++ KO/KO/menu/menu10+/menu3.js
@@ -0,0 +1,28 @@
+// KO/menu/menu10+/menu3.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        meun: [
+            { class: "meun-img", src: "/KO/img/file_58d22b9347eb5.png", title: "平行车保修", url: "/pages/data/vegetable" },
+            { class: "meun-img", src: "/KO/img/file_58d22a97dcec1.png", title: "平行车延保" },
+            { class: "meun-img", src: "/KO/img/file_58d22a98402d8.png", title: "中规车延保" },
+            { class: "meun-img", src: "/KO/img/file_58ed90c5e0149.png", title: "汽车金融服务" },
+        ],
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
