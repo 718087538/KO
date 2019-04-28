ab270e3559649af937d6cfcb576f29594bdb76bf
Kason <718087538@qq.com>
2019-04-28 16:11:54 +0800

    正在编写起步页面

---
+++ KO/KO/list/list30+/list8.js
@@ -0,0 +1,29 @@
+// KO/list/list30+/list8.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        lis: [
+            { class1: "img_2", item: "充值", src: "vip_icon_top-up@2x" },
+            { class1: "img_3", item: "交易明细", src: "my_icon_order@2x" },
+            { class1: "img_4", item: "提现", src: "my_icon_order@2x" },
+
+
+        ],
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
