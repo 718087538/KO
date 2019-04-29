7dd89cc6c3da558d8d3a548c10123b0763e8e5f4
Kason <718087538@qq.com>
2019-04-29 12:06:37 +0800

    基本完成了起步教程

---
+++ KO/KO/order/order30/order1.js
@@ -0,0 +1,28 @@
+// KO/order/order30/order1.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        items: [
+            {},
+            {},
+            {},
+            {},
+        ]
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
